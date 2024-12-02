bbc7745592ec2fc4776787e2fd1f5ecbe962a93a
Jacob Jönsson <jacob.jonsson02@gmail.com>
2024-12-02 17:33:30 +0100

    added error handling to the input arguments

--- StringSearcher/src/StringSearcher.java
+++ StringSearcher/src/StringSearcher.java
@@ -7,22 +7,28 @@ public class StringSearcher {
   
   public static void Main(String[] args) {
 
-    FileParser fileParser = new FileParser(args[1]);
-    List<String> data = new ArrayList<>();
-    try {
-      List<String> lines = fileParser.ReadFromFile();
-      for (String line : lines) {
-        if(line.contains(args[0])) {
-          data.add(line);
+    if (args.length != 3 && args[0] != "search") {
+      System.out.println("Invalid input. Input should be of the format: search <pattern> <file>");
+    } else {
+
+      FileParser fileParser = new FileParser(args[2]);
+      List<String> data = new ArrayList<>();
+      try {
+        List<String> lines = fileParser.ReadFromFile();
+        for (String line : lines) {
+          if(line.contains(args[1])) {
+            data.add(line);
+          }
         }
+      } catch(Exception e) {
+        System.err.println(e);
       }
-    } catch(Exception e) {
-      System.err.println(e);
+  
+      for (String line : data) {
+        System.out.println(line);
+      }
     }
 
-    for (String line : data) {
-      System.out.println(line);
-    }
   }
   
 }
